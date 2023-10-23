57119fd53db355b1c04496b79d9155b20045aeaf
myyxl <22593897+myyxl@users.noreply.github.com>
2023-10-23 13:51:58 +0200

    feat(bmp388): use external library

--- main/main.c
+++ main/main.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include <unicom_transmitter.h>
 #include <esp_log.h>
+#include <driver_bmp388_basic.h>
 
 void app_task() {
-    //bmp388_handle handle = {};
-    //bmp388_init(&handle);
-    //bmp388_sensor_read(&handle);
+    bmp388_basic_init(BMP388_INTERFACE_IIC, BMP388_ADDRESS_ADO_LOW);
+    vTaskDelay(2000 / portTICK_RATE_MS);
 
-    double altitute = 10.15f;
-    double velocity = 4.5f;
-    double acceleration = 1.0f;
-    double temperature = 19.5f;
+    float altitute = 10.15f;
+    float velocity = 4.5f;
+    float acceleration = 1.0f;
+    float temperature = 19.5f;
+    float pressure = 1.0f;
 
     uint8_t *altitute_ptr = (uint8_t*) &altitute;
     uint8_t *velocity_ptr = (uint8_t*) &velocity;
@@ -20,25 +21,23 @@ void app_task() {
     // Init transmitter
     uint8_t* buffer = init_transmitter("telink-esp8266", 0x07, NULL);
 
-    // Fill buffer
-    for (int i = 0; i < 8; i++) {
-        buffer[i] = altitute_ptr[i]; 
-        buffer[i + 8] = velocity_ptr[i]; 
-        buffer[i + 16] = acceleration_ptr[i]; 
-        buffer[i + 24] = temperature_ptr[i]; 
-    }
-
     // Send data in loop
     ESP_LOGI("transmitter", "Start transmitting");
     while(1) {
-        esp_err_t code = transmit("telink-core", 4*8);
+        bmp388_basic_read((float *)&temperature, (float *)&pressure);
+        for (int i = 0; i < 8; i++) {
+            buffer[i] = altitute_ptr[i]; 
+            buffer[i + 4] = velocity_ptr[i]; 
+            buffer[i + 8] = acceleration_ptr[i]; 
+            buffer[i + 16] = temperature_ptr[i]; 
+        }
+        esp_err_t code = transmit("telink-core", 16);
         if(code != ESP_OK) {
             ESP_LOGE("transmitter", "Error code: %x", code);
         }
-        vTaskDelay(100 / portTICK_RATE_MS);
+        vTaskDelay(1000 / portTICK_RATE_MS);
     }
 }
-
 
 void app_main() {
     xTaskCreate(app_task, "app_task", 2048, NULL, 10, NULL);
