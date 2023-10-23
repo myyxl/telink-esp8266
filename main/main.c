b5ab0804b81a23de1acd50bd4fe0d93ea853d208
myyxl <22593897+myyxl@users.noreply.github.com>
2023-10-23 14:15:36 +0200

    refactor(sensor): init default value

--- main/main.c
+++ main/main.c
@@ -2,16 +2,17 @@
 #include <unicom_transmitter.h>
 #include <esp_log.h>
 #include <driver_bmp388_basic.h>
+#include <math.h>
 
 void app_task() {
     bmp388_basic_init(BMP388_INTERFACE_IIC, BMP388_ADDRESS_ADO_LOW);
     vTaskDelay(2000 / portTICK_RATE_MS);
 
-    float altitute = 10.15f;
-    float velocity = 4.5f;
-    float acceleration = 1.0f;
-    float temperature = 19.5f;
-    float pressure = 1.0f;
+    float altitute = 0.0f;
+    float velocity = 0.0f;
+    float acceleration = 0.0f;
+    float temperature = 0.0f;
+    float pressure = 0.0f;
 
     uint8_t *altitute_ptr = (uint8_t*) &altitute;
     uint8_t *velocity_ptr = (uint8_t*) &velocity;
@@ -25,11 +26,11 @@ void app_task() {
     ESP_LOGI("transmitter", "Start transmitting");
     while(1) {
         bmp388_basic_read((float *)&temperature, (float *)&pressure);
-        for (int i = 0; i < 8; i++) {
+        for (int i = 0; i < 4; i++) {
             buffer[i] = altitute_ptr[i]; 
             buffer[i + 4] = velocity_ptr[i]; 
             buffer[i + 8] = acceleration_ptr[i]; 
-            buffer[i + 16] = temperature_ptr[i]; 
+            buffer[i + 12] = temperature_ptr[i]; 
         }
         esp_err_t code = transmit("telink-core", 16);
         if(code != ESP_OK) {
